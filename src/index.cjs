11696ac20bfd39be3880fd90ad2c903a3f5b7ef0
Roland Papp <pappr2006@gmail.com>
2026-01-21 09:04:36 +0100

    add geolocation endpoint to handle latitude and longitude data

--- src/index.cjs
+++ src/index.cjs
@@ -5,9 +5,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+var addresses = []
+
 //endpoints
 app.get('/hello', (req,res)=>{
     return res.status(200).json({data: "sieg ___!"})
+})
+
+app.post('/geolocation', (req, res)=>{
+  const {latitude, longitude} = req.body
+  console.log({latitude, longitude})
+  addresses.push({latitude, longitude})
+
+  return res.status(201).json('ok')
 })
 
 const port = 3333
